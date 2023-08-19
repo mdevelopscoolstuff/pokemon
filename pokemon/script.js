8db571c2da3b12eeac65c9aa066ee348dfd0bb2b
mdevelopscoolstuff <140818077+mdevelopscoolstuff@users.noreply.github.com>
2023-08-19 22:45:44 +0200

    Add files via upload

---
+++ pokemon/script.js
@@ -0,0 +1,45 @@
+var pokemonList = [];
+var pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 7,
+      types: ["grass", "poison"]
+    },
+    {
+      name: "Charmander",
+      height: 6,
+      types: ["fire"]
+    },
+    {
+      name: "Squirtle",
+      height: 5,
+      types: ["water"]
+    }
+  ];
+  
+  console.log(pokemonList);
+  var pokemonList = [
+    {
+      name: "Pikachu",
+      height: 4,
+      types: ["electric"],
+      imageUrl: "https://example.com/pikachu.jpg"
+    },
+    {
+      name: "Squirtle",
+      height: 5,
+      types: ["water"],
+      imageUrl: "https://example.com/squirtle.jpg"
+    },
+    {
+      name: "Charmander",
+      height: 6,
+      types: ["fire"],
+      imageUrl: "https://example.com/charmander.jpg"
+    }
+    
+  ];
+  
+  
+  console.log(pokemonList);
+  
