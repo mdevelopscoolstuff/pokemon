b9e475e3b82b390942bb6cfe05dbf7670de2528f
mdevelopscoolstuff <140818077+mdevelopscoolstuff@users.noreply.github.com>
2023-09-03 11:29:11 +0300

    Update script.js

--- pokemon/script.js
+++ pokemon/script.js
@@ -101,50 +101,8 @@ var pokemonRepository = (function() {
       .catch(function(error) {
         console.error("Error loading Pokémon list:", error);
       });
-  }
-
-  function loadDetails(pokemon) {
-    var url = pokemon.detailsUrl;
-    return fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(details) {
-        pokemon.imageUrl = details.sprites.front_default;
-        pokemon.height = details.height;
-      })
-      .catch(function(error) {
-        console.error("Error loading Pokémon details:", error);
-      });
-  }
-
-  return {
-    getAll: function() {
-      return pokemonList;
-    },
-    add: function(item) {
-    
-
-// Load the list of Pokémon and add them to the pokemonList
-pokemonRepository.LoadList().then(function() {
-  // Now that the list is loaded, you can add list items and details
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
-});
-
 //note 2 task,1.7//
-var pokemonRepository = (function() {
-  var pokemonList = [];
-
-  function addListItem(pokemon) {
-    // ... (previous code)
-  }
-
-  
-
- 
-    return fetch("https://pokeapi.co/api/v2/pokemon/")
+ return fetch("https://pokeapi.co/api/v2/pokemon/")
       .then(function(response) {
         return response.json();
       })
@@ -176,33 +134,6 @@ var pokemonRepository = (function() {
         console.error("Error loading Pokémon details:", error);
       });
   }
-
-  return {
-    getAll: function() {
-      return pokemonList;
-    },
-    add: function(item) {
-      if (typeof item === "object" && item.name && item.detailsUrl) {
-        pokemonList.push(item);
-      } else {
-        console.log("Invalid Pokémon data.");
-      }
-    },
-    addListItem: addListItem,
-    LoadList: loadList,
-    loadDetails: loadDetails
-  };
-})();
-
-// Load the list of Pokémon and their details
-pokemonRepository.LoadList().then(function() {
-  var allPokemons = pokemonRepository.getAll();
-
-  // Loop through the Pokémon list and use addListItem function
-  allPokemons.forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
-});
 //task 1.8 note 1//
 var pokemonRepository = (function() {
   // ... (previous code)
@@ -235,29 +166,11 @@ var pokemonRepository = (function() {
     modalContainer.classList.remove("is-visible");
   }
 
-  // ... (return object)
-
-  // Return additional functions as needed
-  return {
-    // ...
-    showDetails: showDetails
-  };
-})();
-
-// ... (load list and add list items)
 
 // Event listener to close the modal on overlay click
 document.querySelector("#modal-container").addEventListener("click", function(event) {
   var modalContainer = document.querySelector("#modal-container");
   if (event.target === modalContainer) {
-    pokemonRepository.hideModal();
-  }
-});
-
-// Event listener to close the modal on ESC key press
-window.addEventListener("keydown", function(event) {
-  var modalContainer = document.querySelector("#modal-container");
-  if (event.key === "Escape" && modalContainer.classList.contains("is-visible")) {
     pokemonRepository.hideModal();
   }
 });
